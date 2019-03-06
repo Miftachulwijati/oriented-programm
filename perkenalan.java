d5b07aef5a07c88f7e4fb2809e416500f33a1e1c
Miftachulwijati <48273349+Miftachulwijati@users.noreply.github.com>
2019-03-06 14:23:34 +0700

    Miftachul Wijati 4B 17090020

---
+++ perkenalan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan1;
+
+/**
+ *
+ * @author G40
+ */
+public class perkenalan {
+    public static void main(String[] args) {
+        System.out.println("Assalamualaikum");
+        System.out.println("Perkenalkan, saya Miftachul Wijati");
+        System.out.println("Panggil saja Mitta, dari kelas4B");
+    }
+}
